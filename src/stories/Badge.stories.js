05029a0a4041d0a6edbaa06773ce73416ca903f1
Eddie Kao <eddie@5xcampus.com>
2026-01-06 07:20:30 +0800

    feat: 加入 args 展示不同狀態

--- src/stories/Badge.stories.js
+++ src/stories/Badge.stories.js
@@ -5,7 +5,39 @@ const meta = {
   component: Badge
 }
 
-const Default = {}
+const Default = {
+  args: {
+    label: "預設"
+  }
+}
+
+const Primary = {
+  args: {
+    label: "主要",
+    variant: "primary"
+  }
+}
+
+const Success = {
+  args: {
+    label: "成功",
+    variant: "success"
+  }
+}
+
+const Warning = {
+  args: {
+    label: "警告",
+    variant: "warning"
+  }
+}
+
+const Danger = {
+  args: {
+    label: "危險",
+    variant: "danger"
+  }
+}
 
 export default meta
-export { Default }
+export { Default, Primary, Success, Warning, Danger }
