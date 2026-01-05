395ff34aef78c76a292cb6fb6ed84382c44183f6
Eddie Kao <eddie@5xcampus.com>
2026-01-06 07:19:19 +0800

    feat: 建立 Badge 元件

---
+++ src/stories/Badge.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  label: String,
+  variant: {
+    type: String,
+    default: "default"
+  }
+})
+
+const variantClasses = {
+  default: "bg-gray-200 text-gray-700",
+  primary: "bg-blue-500 text-white",
+  success: "bg-green-500 text-white",
+  warning: "bg-yellow-500 text-white",
+  danger: "bg-red-500 text-white"
+}
+</script>
+
+<template>
+  <span
+    class="inline-block px-2 py-1 text-sm rounded"
+    :class="variantClasses[variant]"
+  >
+    {{ label }}
+  </span>
+</template>
